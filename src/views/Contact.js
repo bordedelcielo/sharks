9e91f41fa3270e6d41753d9a3043e00bc6ed46d0
bordedelcielo <christopher.thrutchley@gmail.com>
2022-07-26 12:10:40 -0500

    updated home page

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,5 +1,40 @@
+import { useEffect, useState } from "react"
+
 export default function Contact() {
+    const [color, setColor] = useState('Green')
+    const [count, setCount] = useState(0)
+
+    // runs once
+    useEffect(() => {
+        console.log('Component Mounted')
+
+    }, []) // dependency array
+
+    // runs everytime the component reloads
+    useEffect(() => {
+        console.log('Component Updated')
+    })
+
+    // Run code whenever the value of color changes
+    useEffect(() => {
+        console.log('Color changed to: ' + color)
+        document.body.style.backgroundColor = color
+    }, [color]) //color is a variable that, when changed, causes this useEffect function to run.
+
+    function changeColor() {
+        let colors = ["Blue", "Green", "Orange", "Yellow", "Pink"]
+
+        // Get random color from array
+        setColor(colors[Math.floor(Math.random() * colors.length)])
+    }
+
     return (
-        <h1>Contact</h1>
+        <>
+            <h1>Contact</h1>
+            <p>Count { count }</p>
+            <button onClick={() => setCount(count + 1)}>Increment Counter</button>
+            <p>Color: { color }</p>
+            <button onClick={() => changeColor()}>Change Color</button>
+        </>
     )
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,64 @@
+import { useEffect, useState } from 'react'
+
 export default function Home() {
+    const [pokemonId, setPokemonId] = useState(1)
+    const [pokemon, setPokemon] = useState({})
+    const [increment, setIncrement] = useState(0)
+    const [loadState, setLoadState] = useState('LOADING')
+
+    useEffect(() => {
+        console.log('useEffect triggered by modified increment.')
+        setTimeout(() => {
+            setIncrement(increment + 1)
+        }, 3000)
+    }, [increment])
+
+    useEffect(() => {
+        // https://pokeapi.co/api/v2/pokemon/${id or name}/
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                setPokemon(data)
+                setLoadState('LOADED')
+            })
+    }, [pokemonId])
+
+
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+            <p>Current increment: {increment}</p>
+            <p>Current Pokemon ID: {pokemonId}</p>
+            <div className="pokemon">
+                {
+                    (loadState === 'LOADING') ?
+                    (
+                        <>
+                            <img src={ pokemon.sprites.front_default } alt="" />
+                            <h2>{ pokemon.name }</h2>
+                            <p>Height: { pokemon.height }</p>
+                            <p>Weight: { pokemon.weight }</p>
+                        </>
+                    ) :
+                    (
+                        <p>Loading...</p>
+                    )
+                }
+
+            </div>
+            {
+                (pokemonId > 1) ?
+                <button onClick={ () => setPokemonId(pokemonId-1)}>Previous Pokemon</button>
+                :
+                <></>
+            }
+            {
+                (pokemonId < 10) ?
+                <button onClick = {() => setPokemonId(pokemonId+1)}>Next Pokemon</button>
+                :
+                <></>
+            }
+        </>
     )
 }
